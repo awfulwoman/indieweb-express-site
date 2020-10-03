7e7edbfa678e29ad3c33348b2946c937d98e6d82
Charlie Owen <charlie@sonniesedge.co.uk>
2020-10-03 12:04:43 +0200

    Add filedateformat config setting

--- config.js
+++ config.js
@@ -14,6 +14,7 @@ const config = {
   sitePortExternal: process.env.SITEPORTEXTERNAL || '80',
   siteProtocol: `${process.env['SITEPROTOCOL'] || 'http'}://`,
   siteDomain: process.env['SITEDOMAIN'] || '127.0.0.1',
+  fileDateFormat: "yyyyLLdd't'HHmm",
   globalFields: [
     {
       fieldId: 'guid',
