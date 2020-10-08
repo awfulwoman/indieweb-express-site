f9dc1a9bf65c15cba244d73ecccb178109814321
Charlie Owen <charlie@sonniesedge.co.uk>
2020-10-08 13:33:53 +0200

    Improve formatting

--- utilities/db.js
+++ utilities/db.js
@@ -6,16 +6,13 @@ const is = require('is_js')
 const db = {
   create: async function (type, id, fileContent) {
     try {
-      if (!type || !fileContent || !id)
-        throw new Error('db.create: Missing parameters')
-      if (is.not.string(type) || is.not.string(id))
-        throw new Error('db.create: Parameters must be supplied as strings')
+      if (!type || !fileContent || !id) throw new Error('db.create: Missing parameters')
+      if (is.not.string(type) || is.not.string(id)) throw new Error('db.create: Parameters must be supplied as strings')
 
       let destination = path.join(config.contentRoot, type, id, 'index.md')
 
       fs.stat(destination, (err, stat) => {
-        if (is.not.falsy(err))
-          throw new Error('db.create: File already exists')
+        if (is.not.falsy(err)) throw new Error('db.create: File already exists')
       });
 
       return await fs.promises.writeFile(destination, message, 'utf8')
@@ -27,10 +24,9 @@ const db = {
 
   read: async function (type, id) {
     try {
-      if (!type || !id)
-        throw new Error('db.read: Missing parameters')
-      if (is.not.string(type) || is.not.string(id))
-        throw new Error('db.read: Parameters must be supplied as strings')
+      if (!type || !id) throw new Error('db.read: Missing parameters')
+      if (is.not.string(type) || is.not.string(id)) throw new Error('db.read: Parameters must be supplied as strings')
+      
       let destination = path.join(config.contentRoot, type, id, 'index.md')
 
       return await fs.promises.readFile(destination, { encoding: 'utf8' })
@@ -41,15 +37,13 @@ const db = {
 
   update: async function (type, id, fileContent) {
     try {
-      if (!type || !fileContent || !id)
-        throw new Error('db.update: Missing parameters')
-      if (is.not.string(type) || is.not.string(id))
-        throw new Error('db.update: Parameters must be supplied as strings')
+      if (!type || !fileContent || !id) throw new Error('db.update: Missing parameters')
+      if (is.not.string(type) || is.not.string(id)) throw new Error('db.update: Parameters must be supplied as strings')
 
       let destination = path.join(config.contentRoot, type, id, 'index.md')
+
       fs.stat(destination, (err, stat) => {
-        if (is.falsy(err))
-          throw new Error('db.update: No file exists to update')
+        if (is.falsy(err)) throw new Error('db.update: No file exists to update')
       });
 
       return await fs.promises.writeFile(destination, message, 'utf8')
