b628904b3a823a96fbb642ce66884542e0278438
Charlie Owen <charlie@sonniesedge.co.uk>
2020-10-09 18:03:48 +0200

    Generics

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -8,112 +8,35 @@ const router = express.Router({
     strict: app.get('strict routing')
 });
 
-const files = require('../drivers/files')
+// Models
 const note = require('../models/types/note.model')
 
 // Middleware
-const bodyParser = require('body-parser')
-const urlencodedParser = bodyParser.urlencoded({ extended: true })
-const checkAuthentication = require('../middleware/checkauthentication')
 const asyncHandler = require('express-async-handler');
-// const ErrorHandler = require('../utilities/error-handler')
-const readFile = require('./generics/read-file')
-const readMarkdown = require('./generics/read-markdown')
+const file = require('./generics/file')
+const markdown = require('./generics/markdown')
+const checkAuthentication = require('../middleware/checkauthentication')
 
-const { body, validationResult } = require('express-validator');
-const is = require('is_js');
+// router.get('/notes/', asyncHandler(async (req, res, next) => {
 
-const validateNote = [
-  body('content').isAlphanumeric().trim(),
-  body('images')
-]
+//   let itemObj = await note.read(req.params.id);
 
-router.get('/notes/', asyncHandler(async (req, res, next) => {
+//   res.render('index', {
+//     content: itemObj.content,
+//     meta: itemObj.data,
+//   });
 
-  let itemObj = await note.read(req.params.id);
+// }));
 
-  res.render('index', {
-    content: itemObj.content,
-    meta: itemObj.data,
-  });
-
-}));
-
-router.get('/notes/:id/', readMarkdown(note));
-
-router.get('/notes/create/', [], asyncHandler(async (req, res, next) => {
-  res.render('create/note', {
-    content: `Start creating your note!`,
-    fields: note.fields
-  });
-}));
-
-router.post('/notes/create', [checkAuthentication, urlencodedParser, validateNote], asyncHandler(async (req, res, next) => {
-
-  // Errors?
-  const errors = validationResult(req);
-
-  try {
-
-    if (errors && is.not.empty(errors)) {
-      throw new Error('Validation errors present');
-    }
-
-    // add any missing data
-    // ensureDefaultData() ??
-    if (is.falsy(req.body.meta.created))
-      req.body.meta.created = _global.fields.created.default
-    if (is.falsy(req.body.meta.updated))
-      req.body.meta.updated = _global.fields.updated.default
-    if (is.falsy(req.body.meta.uuid))
-      req.body.meta.uuid = _global.fields.uuid.default
-    if (is.falsy(req.body.meta.id))
-      req.body.meta.id = id || _global.fields.id.default
-
-    // save
-    note.create(meta, content, id)
-
-    // Render
-    res.render('create/note/', {
-      success: true,
-      flash: { type: 'success', message: `Note created` },
-      content: `Your new Note has been created!`,
-      url: `/notes/${req.body.meta.id}/`
-    });
-  } catch (error) {
-    res.render('create/note', {
-      flash: { type: 'error', message: `Note creation failed`, additional: error.toString() },
-      content: `Something went wrong and the note wasn't created. 😭`,
-      errors: errors.toArray()
-    });
-  }
-}));
-
-router.get('/notes/:id/edit/', [], asyncHandler(async (req, res, next) => {
-
-}));
-
-router.post('/notes/:id/edit/', [], asyncHandler(async (req, res, next) => {
-
-}));
-
-router.get('/notes/:id/delete/', [], asyncHandler(async (req, res, next) => {
-
-}));
-
-router.post('/notes/:id/delete/', [], asyncHandler(async (req, res, next) => {
-
-}));
-
-
-router.get('/notes/:id/:file', [], readFile);
-
-router.get('/notes/:id/:file/:size', [], asyncHandler(async (req, res, next) => {
-  // check that note id is valid
-
-  // try to get file
-
-
-}));
+// router.get('/notes/', markdown.read(note));
+router.get('/notes/:id/', markdown.read(note));
+router.get('/notes/create/', [], markdown.create.get(note));
+router.post('/notes/create/', [], markdown.create.post(note));
+router.get('/notes/:id/edit/', [], markdown.update.get(note));
+router.post('/notes/:id/edit/', [], markdown.update.post(note));
+router.get('/notes/:id/delete/', [], markdown.delete.get(note));
+router.post('/notes/:id/delete/', [], markdown.delete.post(note));
+router.get('/notes/:id/:file', [], file.read(note));
+router.get('/notes/:id/:file/:size', [], file.read(note));
 
 module.exports = router;
