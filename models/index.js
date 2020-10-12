e0ff460a15333c77e3e67e7ad8e174d9e3312d8e
Charlie Owen <charlie@sonniesedge.co.uk>
2020-10-12 19:26:38 +0200

    Make recent list sorted

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ let note = require('./note.model')
 let bookmark = require('./bookmark.model')
 let static = require('./static.model')
 let naturalSort = require('javascript-natural-sort')
+const {orderBy} = require('natural-orderby');
 
 const models = [note, bookmark, static]
 
@@ -15,18 +16,31 @@ const warmAll = async () => {
 }
 
 const recent = async () => {
-  let recentItems = []
-  for (let index = 0; index < models.length; index++) {
-    if(models[index].recent) {
-      
-      for (let [key, value] of Object.entries(await models[index].recent())) {
-        // debug(value)
-        recentItems.push(value)
+  try {
+    let recentItems = []
+
+    for (let index = 0; index < models.length; index++) {
+      debug(models[index].modelDir)
+      if(models[index].recent) {
+        
+        for (let [key, value] of Object.entries(await models[index].recent())) {
+          // debug(value)
+          recentItems.push(value)
+        }      
       }
-    
-      return recentItems.sort(naturalSort).slice(0, 20)
     }
+  
+    let sorted = orderBy(
+      recentItems, 
+      [v => v.data.created],
+      ['desc']
+      )
+  
+    return sorted.slice(0, 20)
+  } catch (error) {
+    throw error
   }
+
 }
 
 
