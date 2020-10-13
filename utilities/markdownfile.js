002e4253b9fd97a2da392fa2cafbe4d53f8cd9b3
Charlie Owen <charlie@sonniesedge.co.uk>
2020-10-13 11:07:19 +0200

    Update markdownfile.js

--- utilities/markdownfile.js
+++ utilities/markdownfile.js
@@ -1,6 +1,7 @@
 const outdent = require('outdent')
 const matter = require('gray-matter')
 
+// TODO: rename to "createMarkdown" or similar
 const markdownFile = (meta, content) => {
   return matter.stringify(meta, content)
 }
