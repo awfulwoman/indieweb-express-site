563b93fc4165970c426c892fd6a487cf16af7e08
Charlie Owen <charlie@sonniesedge.co.uk>
2020-10-13 21:07:36 +0200

    Cleanup old files (#16)
    * Delete contentpath.js
    
    * Delete isemptystring.js
    
    * Delete excerpt.js
    
    * Delete contentpath.test.js
    
    * Delete markdownfile.js

--- utilities/markdownfile.js
+++
@@ -1,9 +0,0 @@
-const outdent = require('outdent')
-const matter = require('gray-matter')
-
-// TODO: rename to "createMarkdown" or similar
-const markdownFile = (meta, content) => {
-  return matter.stringify(meta, content)
-}
-
-module.exports = markdownFile
