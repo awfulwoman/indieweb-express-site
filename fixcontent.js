2b1222cdaaff5ac006e4d3f2b437a3f87fe4162f
Charlie Owen <charlie@sonniesedge.co.uk>
2020-10-15 22:04:29 +0200

    Fix content save

--- fixcontent.js
+++ fixcontent.js
@@ -4,57 +4,67 @@
   const config = require('./config')
   const fg = require('fast-glob')
   const path = require('path')
-  const {models} = require('./models')
+  const { models } = require('./models')
 
   // debug(config)
   // debug(process.env)
-  
+
   try {
-    
+
     for (const model of models) {
       // model = models.models.like
       let modelItemList = []
       // debug(model)
       debug(model.modelDir)
       modelItemList = await fg(path.join(config.contentRoot, model.modelDir, '*', 'index.md'))
-      
+
       for (let i = 0; i < modelItemList.length; i++) {
         let id = modelItemList[i].split('/').reverse()[1]
         let item = await model.read(id)
-        debug(`ITEM: ${model.modelDir}/${id}: `)
-        debug(item)
+        // debug(`ITEM: ${model.modelDir}/${id}: `)
+        // debug(item)
 
 
 
         let dataPathId = item.data.path ? item.data.path.split('/').reverse()[0].toString() : null
-        let dataDiskId = id.toString() 
+        let dataDiskId = id.toString()
         let dataFmId = item.data.id ? item.data.id.toString() : null
 
         // if (!dataDiskId) {debug('no disk ID for: ', id)}
         // if (!dataFmId) {debug('no frontmatter ID for: ', id)}
         // if (!dataPathId) {debug('no frontmatter path for: ', id)}
 
-        if (dataPathId === dataFmId && dataPathId === dataDiskId && dataFmId === dataDiskId) {
-          let delete1 = delete item.data.path
-          debug(`${id} deleted frontmatter path?: ${delete1}`)
-          let delete2 = delete item.data.id
-          debug(`${id} deleted frontmatter id?: ${delete2}`)
+        // if ( model.modelDir != 'posts') {
+        //   let delete1 = delete item.data.path
+        //   // debug(`${model.modelDir}/${id} deleted frontmatter path?: ${delete1}`)
+        //   let delete2 = delete item.data.id
+        //   // debug(`${model.modelDir}/${id} deleted frontmatter id?: ${delete2}`)
+        //   delete item.data.type
+
+        // }
+        // if ((item.data.id && item.data.path) && (item.data.id != item.data.path) && model.modelDir != 'posts') {
+        //   // debug('-------------------------------------')
+        //   // debug('DIRTY ITEM')
+        //   // debug('-------------------------------------')
+        //   // debug('item: ', item)
+        //   // debug('id: ', id)
+        // }
+
+        if(item.data.id) {
+          debug(`${model.modelDir}/${id}`)
+        //   delete item.data.type
         }
 
-        if(item.data.type) {
-          delete item.data.type
-        }
-
-        debug(`ITEM: ${model.modelDir}/${id} (after property deletion): `)
-        debug(item)
+        // debug(`ITEM: ${model.modelDir}/${id} (after property deletion): `)
+        // debug(item)
 
 
         // // debug('Dir id: ', id)
         // // debug('Frontmatter id: ', item.data.id)
         // debug('Frontmatter path: ', item.data.path)
         // debug('item.data: ', item.data)
-        debug('item.content defined as: ', typeof(item.content))
-        debug('item.content length: ', item.content.length)
+        // debug('item.content defined as: ', typeof(item.content))
+        // debug('item.content length: ', item.content.length)
         // // debug('-------------------------------------')
 
 
@@ -62,19 +72,19 @@
         let saveId = id.toString()
         let saveData = item.data
 
-        debug('saveData: ', saveData)
-        debug('saveContent: ', saveContent)
-        debug('saveId: ', saveId)
+        // debug('saveData: ', saveData)
+        // debug('saveContent: ', saveContent)
+        // debug('saveId: ', saveId)
 
-        await model.update(saveData, saveContent, saveId)
+        // await model.update(saveData, saveContent, saveId)
 
-        debug(`Updated ${model.modelDir}/${id} `)
+        // debug(`Updated ${model.modelDir}/${id} `)
 
 
       } // end for loop
 
 
-      debug(modelItemList.length)
+      debug(`Length of ${model.modelDir} is ${modelItemList.length}`)
 
 
     } // end for of loop
