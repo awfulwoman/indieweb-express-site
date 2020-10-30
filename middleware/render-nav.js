2ec8ffab722a6ae8600b60baeedd59eb138d8357
Charlie Owen <charlie@sonniesedge.co.uk>
2020-10-30 13:52:11 +0100

    Remove debugging

--- middleware/render-nav.js
+++ middleware/render-nav.js
@@ -2,7 +2,6 @@ const nav = require('../nav')
 const debug = require('debug')('sonniesedge:middleware:renderNav')
 
 const renderNav = function (req,res,next) {
-  debug('req.originalUrl: ', req.originalUrl)
   res.locals.nav = nav(req.originalUrl);
   next();
 }
