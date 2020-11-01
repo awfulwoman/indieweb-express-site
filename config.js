cfcae62d7087f9e91f69f096a33e4630621aa913
Charlie Owen <charlie@sonniesedge.co.uk>
2020-11-01 20:01:38 +0100

    Make env var naming consistent

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ const dataRoot = () => process.env['DATA_DIR'] || path.join(appRoot(), 'example'
 const contentRoot = () => process.env['CONTENT_DIR'] || path.join(appRoot(), 'example', 'content')
 const siteTitle = () => 'sonniesedge'
 const siteDescription = () => 'Charlie Owen and her owned content'
-const sitePort = () => process.env['SITEPORT'] || '3000'
-const sitePortExternal = () => process.env['SITEPORTEXTERNAL'] || '80'
-const siteProtocol = () => `${process.env['SITEPROTOCOL'] || 'http'}://`
-const siteDomain = () => `${process.env['SITEDOMAIN'] || '127.0.0.1'}`
+const sitePort = () => process.env['SITE_PORT'] || '3000'
+const sitePortExternal = () => process.env['SITE_PORT_EXTERNAL'] || '80'
+const siteProtocol = () => `${process.env['SITE_PROTOCOL'] || 'http'}://`
+const siteDomain = () => `${process.env['SITE_DOMAIN'] || '127.0.0.1'}`
 const siteUrl = () => `${siteProtocol()}${siteDomain()}`
 const fileDateFormat = () => `yyyyLLdd't'HHmm`
 
