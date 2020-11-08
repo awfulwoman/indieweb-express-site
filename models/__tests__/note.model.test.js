9cc1b99360258f78fd3c872fd2029649f4dbe319
Charlie Owen <charlie@sonniesedge.co.uk>
2020-11-08 21:02:31 +0100

    Fix tests

--- models/__tests__/note.model.test.js
+++ models/__tests__/note.model.test.js
@@ -11,8 +11,8 @@ let expectedNodeData = {
     type: 'note',
     id: '0000',
     title: 'This is a note',
-    created: '2019-08-26T11:12:22+00:00',
-    changed: '2019-09-24T19:20:44+00:00',
+    created: '2019-08-26T13:12:22.000+02:00',
+    changed: '2019-09-24T21:20:44.000+02:00',
   }
 }
 
