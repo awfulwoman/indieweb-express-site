c0696c03b8fe5ce39318912fca67ae28a0d2f63c
Charlie Owen <charlie@sonniesedge.co.uk>
2020-11-23 17:08:11 +0100

    Disambiguation

--- controllers/disambiguation/index.js
+++ controllers/disambiguation/index.js
@@ -1,16 +1,21 @@
 const debug = require('debug')('sonniesedge:controller:disambiguation')
 const asyncHandler = require('express-async-handler')
+const renderNav = require('../../middleware/render-nav')
+const checkAuthentication = require('../../middleware/check-authentication')
 
 // 🏃‍♀️💨 Express
 const express = require('express')
 const router = express.Router()
 const models = require('../../models')
 
+const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({ extended: true })
+
 // 🖕 Middleware
 // const {controllerArchiveHelper} = require('../utils')
 
 // 🔓 Public routes 
-router.get(`/disambiguation`, asyncHandler(async (req, res, next) => {
+router.get(`/disambiguation`, [renderNav, checkAuthentication], asyncHandler(async (req, res, next) => {
   try {
     res.render('content-create/disambiguation-get', {
       data: { title: 'Disambiguation page - GET' }
@@ -20,7 +25,7 @@ router.get(`/disambiguation`, asyncHandler(async (req, res, next) => {
   }
 }))
 
-router.post(`/disambiguation`, asyncHandler(async (req, res, next) => {
+router.post(`/disambiguation`, [urlencodedParser, renderNav, checkAuthentication], asyncHandler(async (req, res, next) => {
   try {
     
     debug('body: ', req.body)
