c4565f0c2e2a81ba0edd77a1be981f2cb6556774
Charlie Owen <charlie@sonniesedge.co.uk>
2020-11-25 11:16:42 +0100

    Tall

--- controllers/utils/content/create.get.js
+++ controllers/utils/content/create.get.js
@@ -6,22 +6,30 @@ const md = require('../../../utilities/markdown-it')
 const bodyParser = require('body-parser')
 const urlencodedParser = bodyParser.urlencoded({ extended: true })
 const is = require('is_js')
+const tall = require('tall').tall
 
 const createGet = (model, options) => {
   options || (options = {});
 
-  return asyncHandler((req, res, next) => {
+  return asyncHandler(async (req, res, next) => {
     let form_state = {}
+
+
+    // // Unshorten any shortened URLs
+    
+    let resolvedUrl = await tall(form_state['url'])
+
+
 
     form_state['title'] = req.query.title
     form_state['content'] = req.query.text
-    form_state['url'] = req.query.url
+    form_state['url'] = resolvedUrl
     
-    form_state['bookmark_of'] = req.query.url
-    form_state['like_of'] = req.query.url
-    form_state['quote_of'] = req.query.url
-    form_state['reply_to'] = req.query.url
-    form_state['repost_of'] = req.query.url
+    form_state['bookmark_of'] = resolvedUrl
+    form_state['like_of'] = resolvedUrl
+    form_state['quote_of'] = resolvedUrl
+    form_state['reply_to'] = resolvedUrl
+    form_state['repost_of'] = resolvedUrl
 
     res.render(options.template || `content-create/types/${model.modelDir}`, {
       data: { title: `${model.modelDir} creation` },
