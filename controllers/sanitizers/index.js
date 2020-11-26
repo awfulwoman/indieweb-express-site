1a618a5cc79546a1cc5096d533be1e6ee7bc6b69
Charlie Owen <charlie@sonniesedge.co.uk>
2020-11-26 20:49:10 +0100

    Tags

--- controllers/sanitizers/index.js
+++ controllers/sanitizers/index.js
@@ -1,7 +1,11 @@
 const { body } = require('express-validator')
 
 module.exports = [
-  body('tags').optional({checkFalsy: true}).toArray(),
+  body('tags').optional({checkFalsy: true}).customSanitizer(value => {
+    return value.split(',').map(tag => {
+      return tag.trim()
+    })
+  }),
   body('private').optional({checkFalsy: true}).toBoolean(),
   body('hide_from_feed').optional({checkFalsy: true}).toBoolean(),
   body('hide_from_index').optional({checkFalsy: true}).toBoolean()  
