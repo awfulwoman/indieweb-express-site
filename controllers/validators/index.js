8bf36811bbb10f6b1ffb97c283f1136aa78d60de
Charlie Owen <charlie@sonniesedge.co.uk>
2020-11-30 10:13:07 +0100

    Slugs all over me

--- controllers/validators/index.js
+++ controllers/validators/index.js
@@ -2,6 +2,8 @@ const { body } = require('express-validator')
 
 module.exports = [
   body('content').optional({checkFalsy: true}),
+  body('slug').optional({checkFalsy: true}),
+  body('strapline').optional({checkFalsy: true}),
   body('place[latlng]').optional({checkFalsy: true}).isLatLong(),
   body('bookmark_of').optional({checkFalsy: true}).isURL(),
   body('like_of').optional({checkFalsy: true}).isURL(),
