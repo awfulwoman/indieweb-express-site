1c8b1c9c1456c694638d386e60ffeadd5b1f47e4
Charlie Owen <charlie@sonniesedge.co.uk>
2020-12-02 15:40:33 +0100

    Scripting

--- build.sh
+++ build.sh
@@ -3,21 +3,16 @@
 APPDIR=$1
 cd $APPDIR
 
-echo "Copying env vars..."
-# cp ../.env .env
-
 echo "Remove existing node_modules..."
 rm -rf node_modules
 
 echo "Building app..."
 npm ci
 
-# Build assets
-
+echo "Building assets..."
 npm run build:css
 
-# Handle server process
-
+echo "Restarting server process..."
 pm2 stop ecosystem.config.js
 pm2 delete ecosystem.config.js
 pm2 start ecosystem.config.js
