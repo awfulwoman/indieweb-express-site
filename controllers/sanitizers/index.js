a339bae8d9cdc1bfc9421995a98e0d65d1ee6463
Charlie Owen <charlie@sonniesedge.co.uk>
2020-12-10 20:57:33 +0100

    Tags

--- controllers/sanitizers/index.js
+++ controllers/sanitizers/index.js
@@ -1,13 +1,15 @@
 const debug = require('debug')('sonniesedge:controllers:sanitizers')
 const { body } = require('express-validator')
+const is = require('is_js')
 
 module.exports = [
-  body('tags').optional({checkFalsy: true}).customSanitizer(value => {
-    debug('tags')
+  body('tags').customSanitizer(value => {
+    debug('Tags:', value)
+    if (is.falsy(value)) return false
     return value.split(',').map(tag => {
       return tag.trim()
     })
-  }),
+  }).optional({checkFalsy: true}),
   body('content').optional({checkFalsy: true}).trim(),
   body('private').optional({checkFalsy: true}).toBoolean(),
   body('hide_from_feed').optional({checkFalsy: true}).toBoolean(),
