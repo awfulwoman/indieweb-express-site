3f8376175adac7c7f813dfab8d83232eb55c9619
Charlie Owen <charlie@sonniesedge.co.uk>
2020-12-11 11:52:10 +0100

    Ensure tags and latlng become arrays

--- controllers/sanitizers/index.js
+++ controllers/sanitizers/index.js
@@ -2,14 +2,17 @@ const debug = require('debug')('sonniesedge:controllers:sanitizers')
 const { body } = require('express-validator')
 const is = require('is_js')
 
+const string2array = (value) => {
+  debug('Tags:', value)
+  if (is.falsy(value)) return false
+  return value.split(',').map(tag => {
+    return tag.trim()
+  })
+}
+
 module.exports = [
-  body('tags').customSanitizer(value => {
-    debug('Tags:', value)
-    if (is.falsy(value)) return false
-    return value.split(',').map(tag => {
-      return tag.trim()
-    })
-  }).optional({checkFalsy: true}),
+  body('tags').customSanitizer(value => string2array(value)).optional({checkFalsy: true}),
+  body('place[latlng]').customSanitizer(value => string2array(value)).optional({checkFalsy: true}),
   body('content').optional({checkFalsy: true}).trim().stripLow(true),
   body('private').optional({checkFalsy: true}).toBoolean(),
   body('hide_from_feed').optional({checkFalsy: true}).toBoolean(),
