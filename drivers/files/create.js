a61dfd6d28c34d54ae5af66665dc862994b5cd76
Charlie Owen <charlie@sonniesedge.co.uk>
2020-12-20 14:10:58 +0100

    Modularise files

---
+++ drivers/files/create.js
@@ -0,0 +1,17 @@
+// create: async function (type, id, fileContent) {
+//   // try {
+//   //   if (!type || !fileContent || !id) throw new Error('markdown.create: Missing parameters')
+//   //   if (is.not.string(type) || is.not.string(id)) throw new Error('markdown.create: Parameters must be supplied as strings')
+
+//   //   let destination = path.join(config.contentRoot(), type, id, 'index.md')
+
+//   //   fs.stat(destination, (err, stat) => {
+//   //     if (is.not.falsy(err)) throw new Error('markdown.create: File already exists')
+//   //   });
+
+//   //   return await fs.promises.writeFile(destination, message, 'utf8')
+//   // } catch (error) {
+//   //   // TODO Add to error log
+//   //   return Promise.reject(error)
+//   // }
+// },
---
+++ drivers/files/delete.js
@@ -0,0 +1,15 @@
+// delete: async function (type, id) {
+//   // try {
+//   //   if (!type || !id)
+//   //     throw new Error('markdown.del: Missing parameters')
+//   //   if (is.not.string(type) || is.not.string(id))
+//   //     throw new Error('markdown.del: Parameters must be supplied as strings')
+
+//   //   let destination = path.join(config.contentRoot(), location, id, 'index.md')
+
+//   //   return await fs.promises.unlink(destination)
+//   // } catch (error) {
+//   //   return Promise.reject(error)
+//   // }
+// }
+// // 
