dab15c1e259b7fe71161c0919a245393ac114ac7
Chazza <github@whalecoiner.com>
2021-02-09 13:54:36 +0100

    Fix eslinterrors in item create controller

--- controllers/content/create.get.js
+++ controllers/content/create.get.js
@@ -1,47 +1,47 @@
 const debug = require('debug')('indieweb-express-site:controllers:utils:content:createGet')
 const asyncHandler = require('express-async-handler')
-const bodyParser = require('body-parser')
-const urlencodedParser = bodyParser.urlencoded({ extended: true })
 const is = require('is_js')
 const tall = require('tall').tall
 
-const AppError = require('../../utilities/app-error')
-const md = require('../../utilities/markdown-it')
 const config = require('../../config')
-const { resolveAlias } = require('../../models/types/article.model')
 
-const createGet = (model, options) => {
-  options || (options = {});
+const createGet = (model, options = {}) => {
+  return asyncHandler(async (req, res) => {
+    const formState = {}
 
-  return asyncHandler(async (req, res, next) => {
-    let form_state = {}
+    const resolvedUrl = req.query.url && is.url(req.query.url) ? await tall(req.query.url) : undefined // Unshorten any shortened URLs
+    const resolvedTitle = req.query.title && is.string(req.query.title) ? req.query.title : undefined
+    const resolvedText = req.query.text && is.string(req.query.text) ? req.query.text : undefined
 
-    // Unshorten any shortened URLs
-    let resolvedUrl = req.query.url ? await tall(req.query.url) : undefined
+    debug('resolvedUrl: ', resolvedUrl)
+    debug('resolvedTitle: ', resolvedTitle)
+    debug('resolvedText: ', resolvedText)
 
-    form_state['title'] = req.query.title
-    form_state['content'] = req.query.text
-    form_state['url'] = resolvedUrl
+    formState.title = resolvedTitle
+    formState.content = resolvedText
+    formState.url = resolvedUrl
 
-    form_state['bookmark_of'] = resolvedUrl
-    form_state['like_of'] = resolvedUrl
-    form_state['quote_of'] = resolvedUrl
-    form_state['reply_to'] = resolvedUrl
-    form_state['repost_of'] = resolvedUrl
+    formState.bookmark_of = resolvedUrl
+    formState.like_of = resolvedUrl
+    formState.quote_of = resolvedUrl
+    formState.reply_to = resolvedUrl
+    formState.repost_of = resolvedUrl
 
-    let syndicationSilosMissingObj = Array.from(config.silos(), x => {
+    // Convert config array of silos into an array of objects
+    const syndicationSilosMissingObj = Array.from(config.silos(), x => {
       return { id: x }
     })
 
+    // Check to see if any form items should be prefilled
     syndicationSilosMissingObj.forEach(silo => {
       if (silo.id === 'twitter') {
-        if (resolvedUrl.includes('twitter.com')) form_state['syndicate_to_twitter'] = silo.syndicate = true
+        if (resolvedUrl.includes('twitter.com')) formState.syndicate_to_twitter = silo.syndicate = true
       }
     })
 
     res.render(options.template || `content-create/types/${model.id}`, {
-      data: { title: `Create a new ${model.id}` },
-      state: form_state,
+      data: { title: `Create new ${model.id}` },
+      state: formState,
       syndicationSilosMissing: syndicationSilosMissingObj
     })
   })
