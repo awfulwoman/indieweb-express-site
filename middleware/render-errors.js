9051e3aea6a05bddf7f38cc09fd0f7889a06ded6
Chazza <github@whalecoiner.com>
2021-02-10 09:23:48 +0100

    Show error debug messages

--- middleware/render-errors.js
+++ middleware/render-errors.js
@@ -2,7 +2,7 @@ const debug = require('debug')('indieweb-express-site:middleware:renderErrors')
 const md = require('../utilities/markdown-it')
 
 const renderErrors = (err, req, res, next) => {
-  // if (process.env['DEBUG']) debug(err)
+  if (process.env['DEBUG']) debug(err)
   const { statusCode, message, rawError } = err;
   res.status(statusCode || 500);
   res.render('error', {
