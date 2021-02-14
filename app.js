50a6ba97054d4b8ff8ec3033ec42412b835d65f5
Chazza <github@whalecoiner.com>
2021-02-14 22:43:05 +0100

    Update twitter CSP

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ try {
   app.use(helmet({
     contentSecurityPolicy: {
       directives: {
-        defaultSrc: ["'self'", 'blob:', 'blob'],
-        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://platform.twitter.com/widgets.js"],
+        defaultSrc: ["'self'", 'blob:', 'blob', "https://platform.twitter.com"],
+        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
         styleSrc: ["'self'"],
         fontSrc: ["'self'"],
         connectSrc: ["'self'"]
