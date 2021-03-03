ec43a9758300285fb5a33e48864e0910697e5230
Chazza <74922321+whalecoiner@users.noreply.github.com>
2021-03-03 18:21:36 +0100

    Separate controllers and routers (#43)
    * Fix lint errors
    
    * oh god why
    
    * debug
    
    * Fix webmentions

--- middleware/render-errors.js
+++ middleware/render-errors.js
@@ -2,15 +2,20 @@ const debug = require('debug')('indieweb-express-site:middleware:renderErrors')
 const md = require('../utilities/markdown-it')
 
 const renderErrors = (err, req, res, next) => {
-  if (process.env['DEBUG']) debug(err)
-  const { statusCode, message, rawError } = err;
-  res.status(statusCode || 500);
-  res.render('error', {
-    status: 'error',
-    statusCode: statusCode || 500,
-    message: md.render(message) || null,
-    rawError: rawError || null
-  })
+  if (process.env['DEBUG']) debug('ERROR DEBUG:', err)
+  let { statusCode, message, rawError } = err
+  if (message) {message = md.render(message)} else { message = null }
+  // if (statusCode === 404 || statusCode === '404') {
+  //   next()
+  // } else {
+    res.status(statusCode || 500)
+    res.render('error', {
+      status: 'error',
+      statusCode: statusCode || 500,
+      contentHtml: message,
+      rawError: rawError || null
+    })
+  // }
 }
 
 module.exports = renderErrors
