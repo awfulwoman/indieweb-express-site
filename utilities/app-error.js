a728284288b5f2b9b41e470fb6752f51bf3c0b92
Chazza <github@whalecoiner.com>
2021-03-07 19:33:02 +0100

    temp disable again

--- utilities/app-error.js
+++ utilities/app-error.js
@@ -1,15 +1,15 @@
 const debug = require('debug')('indieweb-express-site:utilities:AppError')
-const logger = require('../config.winston')
+// const logger = require('../config.winston')
 
 // Shows a debug message for a throw error
 // Emits a statusCode, message, and rawError that can be rendered by other functions
 
 class AppError extends Error {
   constructor(statusCode, message, rawError) {
-    logger.error({
-      message: message || 'NO MESSAGE SUPPLIED',
-      error: rawError || 'NO ERROR SUPPLIED'
-    })
+    // logger.error({
+    //   message: message || 'NO MESSAGE SUPPLIED',
+    //   error: rawError || 'NO ERROR SUPPLIED'
+    // })
     super()
     this.statusCode = statusCode
     this.message = message
