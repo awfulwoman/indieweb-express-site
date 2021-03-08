eb730f64ae83ce26a8fbfb53c3a09b953f0aec6b
Chazza <github@whalecoiner.com>
2021-03-08 14:59:48 +0100

    Add admin controls back doh

--- middleware/check-authentication.js
+++ middleware/check-authentication.js
@@ -1,7 +1,9 @@
 const config = require('../config')
 
 const checkAuthentication = function (req, res, next) {
-  if (req.isAuthenticated() || process.env.DEBUG) {
+  // if (req.isAuthenticated() || process.env.DEBUG) {
+  if (req.isAuthenticated()) {
+    res.locals.debug = true
     next()
   } else {
     res.redirect(config.siteLoginPath() + '?redirectPath=' + req.route.path);
--- middleware/render-debug.js
+++ middleware/render-debug.js
@@ -2,7 +2,6 @@ const renderDebug = (req, res, next) => {
 
   if(req.header('x-debug') === process.env['DEBUG_TOKEN'] || process.env['DEBUG'] === true) {
     res.locals.debug = true
-    res.locals.admin = true
   }
 
   next()
