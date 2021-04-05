3b3aaf552f6299a4371f2a216b4c7d05af43bbc0
Chazza <github@whalecoiner.com>
2021-04-05 16:40:59 +0200

    Slim page checker

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,39 @@
 import twttr from 'twitter-text'
-import MarkdownIt from 'markdown-it'
-import MarkdownItRegexp from 'markdown-it-regexp'
-let replacer = MarkdownItRegexp(
-  // regexp to match
-  /(^|[^@\w])@(\w{1,15})\b/g,
+// import MarkdownIt from 'markdown-it'
+// import MarkdownItRegexp from 'markdown-it-regexp'
+// let replacer = MarkdownItRegexp(
+//   // regexp to match
+//   /(^|[^@\w])@(\w{1,15})\b/g,
 
-  // this function will be called when something matches
-  function(match, utils) {
-    var url = 'https://twitter.com/' + match[2]
-    console.log('match:', utils.escape(match[2]))
-    return `<a href="${utils.escape(url)}">@${utils.escape(match[2])}</a>`
-  }
-)
-let md = MarkdownIt().use(replacer)
-
+//   // this function will be called when something matches
+//   function(match, utils) {
+//     var url = 'https://twitter.com/' + match[2]
+//     console.log('match:', utils.escape(match[2]))
+//     return `<a href="${utils.escape(url)}">@${utils.escape(match[2])}</a>`
+//   }
+// )
+// let md = MarkdownIt().use(replacer)
 
 const source = document.getElementById('content')
 
 const inputHandler = function(e) {
-  let sourceTxt = e.target.value
-  // let markdown = md.render(e.target.value)
-  // var result = twttr.parseTweet(markdown)
-  var arrayOfLines = sourceTxt.split("\n")
-  let toParse = []
+  // let sourceTxt = e.target.value
+  // // let markdown = md.render(e.target.value)
+  // // var result = twttr.parseTweet(markdown)
+  // var arrayOfLines = sourceTxt.split("\n")
+  // let toParse = []
 
-  arrayOfLines.forEach(line => {
-    console.log('line:', line)
-    let parsed = md.renderInline(line)
-    toParse.push(parsed)
-  })
+  // arrayOfLines.forEach(line => {
+  //   console.log('line:', line)
+  //   let parsed = md.renderInline(line)
+  //   toParse.push(parsed)
+  // })
 
-  var result = twttr.parseTweet(toParse.join('\n'))
-  if (result.valid === false) document.getElementById('tweetable').textContent = 'false'
-  if (result.valid === true) document.getElementById('tweetable').textContent = 'true'
-  console.log(toParse.join('\n'))
+  // var result = twttr.parseTweet(toParse.join('\n'))
+  
+  if (!twttr.parseTweet(e.target.value)) document.getElementById('tweetable').textContent = 'Not tweetable'
+  if (twttr.parseTweet(e.target.value)) document.getElementById('tweetable').textContent = 'Tweetable'
+  // console.log(toParse.join('\n'))
   // console.log(result)
 }
 
