0b9a32c3249ed26ecf19315e1b72c806f722a511
Chazza <github@whalecoiner.com>
2021-04-07 19:07:26 +0200

    Make sure moficiation date is honoured

--- controllers/content/shared/metadata.js
+++ controllers/content/shared/metadata.js
@@ -9,10 +9,11 @@ const metadata = async (data, renderMessages = [], options = {}) => {
     data.created = tempCurrentDate.toISO()
     renderMessages.push(`Created: ${data.created}`)
   }
-  if (!data.modified) {
-    data.modified = tempCurrentDate.toISO()
-    renderMessages.push(`Modified: ${data.modified}`)
-  }
+
+  // Change this on every modification
+  data.modified = tempCurrentDate.toISO()
+  renderMessages.push(`Modified: ${data.modified}`)
+
   if (!data.guid) {
     data.guid = uuid.v4()
     renderMessages.push(`GUID: ${data.guid}`)
