024f14da3438193b9d881dec938f5f563a3f2030
Chazza <github@whalecoiner.com>
2021-05-27 19:16:29 +0200

    Get fucked, Tweety bird

--- models/utils/add-tweetable-content.js
+++ models/utils/add-tweetable-content.js
@@ -1,121 +1,181 @@
-const debug = require('debug')('indieweb-express-site:models:utils:addTwitter')
+const debug = require('debug')('indieweb-express-site:models:utils:addTweetableContent')
 const twttr = require('twitter-text')
 
 const { md, isContentEmpty, cleanContent, quoteSafely } = require('../../utilities')
 
+const tweetableIndiewebFields = ['quote_of', 'bookmark_of']
+
 
 // Does the first object contain a property that matches one of the keys in the second array?
-const relevantUrl = (resultObject, tweetableIndiewebFields) => { 
+const createIndiewebUrlObj = (resultObject, tweetableIndiewebFields) => { 
   for (const field of tweetableIndiewebFields) {
     if (resultObject.data[field] !== undefined) return {
       fieldValue: resultObject.data[field],
       fieldName: field
     }
   }
+  return false
+}
+
+const isTweetable = (content) => {
+  return (twttr.parseTweet(content).valid && !content.includes(']('))
 }
 
 
-// Add an object that contains directly tweetable content
-// All sanitized and safely tweetable.
-const addTweetableContent = (resultObject, options = {}) => {
-  const tweetableIndiewebFields = ['quote_of', 'bookmark_of']
 
-  let tweetableObj = relevantUrl(resultObject, tweetableIndiewebFields)
+const createTweetableObj = (resultObject) => {
+  let indiewebUrlObj = createIndiewebUrlObj(resultObject, tweetableIndiewebFields)
+  let url = indiewebUrlObj.fieldValue
 
   // ---------------------------------------
-  // OPTION 1. USE DEDICATED TWITTER CONTENT
+  // OPTION 1. USE DEDICATED TWITTER CONTENT + indieweb URL)
+  // ---------------------------------------
+  // TODO: ensure that twitter_content is validated as tweetable when saving form
+  // If there is a Twitter content field then see if:
+  // - it's a valid tweet
+  // - it contains no Markdown links
+
+  if (indiewebUrlObj && resultObject.content_twitter && !isContentEmpty(resultObject.content_twitter)) {
+    // If there is separate content then link directly to this item's URL, not to the remote indieweb URL.
+    if (!isContentEmpty(resultObject.content)) url = resultObject.url
+    let content = cleanContent(`${resultObject.content_twitter}\n${url}`)
+    if (isTweetable(content)) {
+      return {
+        markdown: content,
+        html: md.render(content),
+        plain: content,
+        generatedFrom: 'TWITTER_CONTENT'
+      }
+    }
+  }
+
+
+  // ---------------------------------------
+  // OPTION X. USE DEDICATED TWITTER CONTENT
   // ---------------------------------------
   // TODO: ensure that twitter_content is validated as tweetable when saving form
   // If there is a Twitter content field then see if:
   // - it's a valid tweet
   // - it contains no Markdown links
   if (resultObject.content_twitter && !isContentEmpty(resultObject.content_twitter)) {
-    if (resultObject.data && resultObject.data.title) {
-      let contentUrl = cleanContent(`${resultObject.content_twitter}\n${tweetableObj.fieldValue}`)
-      if (twttr.parseTweet(contentUrl).valid && !contentUrl.includes('](')) {
-        if (!resultObject.extended) resultObject.extended = {}
-        resultObject.extended.twitter = {
-          markdown: contentUrl,
-          html: md.render(contentUrl)
-        }
-        return resultObject
+    let contentTwitter = cleanContent(resultObject.content_twitter)
+    if (isTweetable(contentTwitter)) {
+      return {
+        markdown: contentTwitter,
+        html: md.render(contentTwitter),
+        plain: contentTwitter,
+        generatedFrom: 'TWITTER_CONTENT'
       }
     }
   }
 
   // ---------------------------------------
-  // OPTION 2. TWEET NORMAL CONTENT
+  // OPTION X. TWEET NORMAL CONTENT + indieweb URL < 280 chars
   // ---------------------------------------
   // If there is a normal content field then see if:
   // - it's a valid tweet
   // - it contains no Markdown links
-  if (resultObject.contentFormats && resultObject.contentFormats.markdown && !isContentEmpty(resultObject.contentFormats.markdown)) {
-    // debug('Found resultObject.content')
-    if (resultObject.data && resultObject.data.title) {
-      let combinedContentUrlTitle = cleanContent(`${resultObject.content.markdown}\n${quoteSafely(resultObject.data.title)}${tweetableObj ? '\n' + tweetableObj.fieldValue : ''}`)
-      if (twttr.parseTweet(combinedContentUrlTitle).valid && !combinedContentUrlTitle.includes('](')) {
-        // debug('resultObject.content.markdown (with title)', combinedContentUrlTitle)
-        resultObject.twitterFormats = {
-          markdown: combinedContentUrlTitle,
-          html: md.render(combinedContentUrlTitle)
-        }
-        return resultObject
-      }
-    }
+  if (indiewebUrlObj && resultObject.content && !isContentEmpty(resultObject.content)) {
+      let cleanContentTitleUrl = cleanContent(`${resultObject.content}${resultObject.type === 'bookmark' ? quoteSafely(resultObject.data.title) : ''}\n${url}`)
+      let cleanContentUrl = cleanContent(`${resultObject.content}${url ? '\n' + url : ''}`)
 
-    let combinedContentUrl = cleanContent(`${resultObject.content.markdown}${tweetableObj ? '\n' + tweetableObj.fieldValue : ''}`)
-    if (twttr.parseTweet(combinedContentUrl).valid && !combinedContentUrl.includes('](')) {
-      // debug('resultObject.content.markdown', combinedContentUrl)
-      resultObject.twitterFormats = {
-        markdown: combinedContentUrl,
-        html: md.render(combinedContentUrl)
+      // If it can be tweeted with the content, the item title intact, and a URL, then do so
+      if (isTweetable(cleanContentTitleUrl)) {
+        return {
+          markdown: cleanContentTitleUrl,
+          html: md.render(cleanContentTitleUrl),
+          plain: cleanContentTitleUrl,
+          generatedFrom: 'CONTENT'
+        }
       }
-      return resultObject
+
+      // If it can be tweeted with the content and URL, then do so
+      if (isTweetable(cleanContentUrl)) {
+        return{
+          markdown: cleanContentTitleUrl,
+          html: md.render(cleanContentTitleUrl),
+          plain: cleanContentTitleUrl,
+          generatedFrom: 'CONTENT'
+        }
+      }    
+  }
+
+  // ---------------------------------------
+  // OPTION X. TWEET NORMAL CONTENT < 280 chars
+  // ---------------------------------------
+  // If there is just a regular old content field then see if:
+  // - it's a valid tweet
+  // - it contains no Markdown links
+  if (resultObject.content && !isContentEmpty(resultObject.content)) {
+    let normalContent = cleanContent(resultObject.content)
+    if (isTweetable(normalContent)) {
+      return {
+        markdown: normalContent,
+        html: md.render(normalContent),
+        plain: normalContent,
+        generatedFrom: 'CONTENT'
+      }
     }
   }
   
+
+
+
   // -----------------------------------------------------
-  // OPTION 3. GENERIC TWEET POINTING TO 3RD-PARTY URL
+  // OPTION 4. GENERIC TWEET POINTING TO 3RD-PARTY URL
   // -----------------------------------------------------
-  // if there is content that is tweetable then link directly to th
-  // for each indieweb field
-  // debug('Both twitter and content are empty')
+  let genericContent = ''
+  if (indiewebUrlObj) {
+    switch (indiewebUrlObj.fieldName) {
+      case 'quote_of':
+        genericContent = 'I found this Tweet interesting.'
+        break
 
-  // confirm that combinedContentUrl content and indieweb field is tweetableObj
-  let markdownContent = ''
-  if (tweetableObj) {
-    // debug('tweetableObj')
+      case 'bookmark_of':
+        genericContent = '🔖 ' + resultObject.data.title
+        break
 
-    switch (tweetableObj.fieldName) {
-    case 'quote_of':
-      markdownContent = 'I found this Tweet interesting.'
-      break
-
-    case 'bookmark_of':
-      markdownContent = '🔖 ' + resultObject.data.title
-      break
-
-    default:
-      markdownContent = 'I found this URL interesting.'
-      break
+      default:
+        genericContent = 'I found this URL interesting.'
+        break
     }
   }
 
-  let combinedContentUrl = cleanContent(`${markdownContent}${tweetableObj ? '\n' + tweetableObj.fieldValue : ''}`)
-  if (twttr.parseTweet(combinedContentUrl).valid) {
-    resultObject.twitterFormats = {
+  let combinedContentUrl = cleanContent(`${genericContent}${indiewebUrlObj ? '\n' + indiewebUrlObj.fieldValue : ''}`)
+  if (isTweetable(combinedContentUrl)) {
+    return {
       markdown: combinedContentUrl,
-      html: md.render(combinedContentUrl)
+      html: md.render(combinedContentUrl),
+      plain: combinedContentUrl,
+      generatedFrom: 'FALLBACK'
     }
-    return resultObject
   }
 
 
   // -----------------------------------------------------
-  // OPTION 4. GENERIC TWEET POINTING TO ORIGINAL ITEM
+  // OPTION 5. GENERIC TWEET POINTING TO ORIGINAL ITEM?
   // -----------------------------------------------------
 
+  let fallbackContent = `I posted something, dickheads.\n${resultObject.url}`
+  return {
+    markdown: fallbackContent,
+    html: md.render(fallbackContent),
+    plain: fallbackContent,
+    generatedFrom: 'FINAL_FALLBACK'
+  }
 
+}
+
+
+// Add an object that contains directly tweetable content
+// All sanitized and safely tweetable.
+const addTweetableContent = (resultObject, options = {}) => {
+  let result = createTweetableObj(resultObject)
+
+  if (!result) return resultObject
+
+  if (!resultObject.extended) resultObject.extended = {}
+  resultObject.extended.twitter = result
 
   return resultObject
 }
