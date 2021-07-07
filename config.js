c78c4bfe5db6f35df607c934473e74f5aadcc55b
Chazza <github@whalecoiner.com>
2021-07-07 21:59:50 +0200

    adasd

--- config.js
+++ config.js
@@ -3,18 +3,18 @@ const appRootPackage = require('app-root-path');
 const path = require('path')
 
 const appRoot = () => appRootPackage.path
-const dataRoot = () => process.env['DATA_DIR'] || path.join(appRoot(), 'example', 'data')
-const contentRoot = () => process.env['CONTENT_DIR'] || path.join(appRoot(), 'example', 'content')
-const logDir = () => process.env['LOG_DIR'] || path.join(appRoot(), 'log')
+const dataRoot = () => process.env['APP_DATA_DIR'] || path.join(appRoot(), 'example', 'data')
+const contentRoot = () => process.env['APP_DATA_DIR'] || path.join(appRoot(), 'example', 'content')
+const logDir = () => process.env['APP_LOG_DIR'] || path.join(appRoot(), 'log')
 const siteTitle = () => 'whalecoiner'
 const siteDescription = () => 'Charlie and her content'
 const sitePort = () => process.env['SITE_PORT'] || '3000'
 const sitePortExternal = () => process.env['SITE_PORT_EXTERNAL'] || '80'
-const siteProtocol = () => `${process.env['SITE_PROTOCOL'] || 'http'}://`
-const siteDomain = () => `${process.env['SITE_DOMAIN'] || '127.0.0.1'}`
+const siteProtocol = () => `${process.env['APP_PROTOCOL'] || 'http'}://`
+const siteDomain = () => `${process.env['APP_DOMAIN'] || '127.0.0.1'}`
 const siteUrl = () => `${siteProtocol()}${siteDomain()}`
 const siteAuthor = () => 'Charlie Owen'
-const siteLoginPath = () =>  process.env['SITE_LOGIN_PATH'] || '/login'
+const siteLoginPath = () =>  process.env['APP_LOGIN_PATH'] || '/login'
 const fileDateFormat = () => `yyyyLLdd't'HHmmss`
 const keyboardCat = () => process.env['KEYBOARD_CAT'] || 'keyboardcatgetoffyoubastard'
 const twitterConsumerKey = () => process.env['TWITTER_CONSUMER_KEY']
